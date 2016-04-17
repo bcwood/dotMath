54f302578cd27bc0f0ebe46d25aaa2af0051d6a0
Brandon Wood <bcwood@uaa.alaska.edu>
2016-04-16 23:18:12 -0800

    removed = operator, as there's no easy way to distinguish between that and assignment operator

--- dotMath/EquationCompiler.cs
+++ dotMath/EquationCompiler.cs
@@ -361,7 +361,6 @@ namespace dotMath
 			_operators.Add(">=", new COperator((x, y) => { if (x >= y) return 1; else return 0; }));
 			_operators.Add("<", new COperator((x, y) => { if (x < y) return 1; else return 0; }));
 			_operators.Add("<=", new COperator((x, y) => { if (x <= y) return 1; else return 0; }));
-			_operators.Add("=", new COperator((x, y) => { if (x == y) return 1; else return 0; }));
 			_operators.Add("==", new COperator((x, y) => { if (x == y) return 1; else return 0; }));
 			_operators.Add("<>", new COperator((x, y) => { if (x != y) return 1; else return 0; }));
 			_operators.Add("!=", new COperator((x, y) => { if (x != y) return 1; else return 0; }));
